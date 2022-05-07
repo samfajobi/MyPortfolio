b5787953f35982ba6fb7b788e21d3880feb1c771
Sammy <segzyfaj1@gmail.com>
2022-05-07 16:37:27 +0100

    A new Commit

--- FolioBackEnd/src/server.js
+++ FolioBackEnd/src/server.js
@@ -1,8 +1,10 @@
+const dotenv = require("dotenv")
 const express = require('express');
 const nodemailer = require('nodemailer');
 
 
 
+dotenv.config();
 
 const app = express()
 
@@ -21,8 +23,41 @@ app.post("/api/form", ( req, res ) => {
     let data = req.body
 
     let transporter = nodemailer.createTransport({
+        service: Gmail,
+        port: 465,
+        auth: {
+            user: process.env.emailAddress,
+            pass: process.env.password
+        }
 
     })
+
+
+    let mailOptions = {
+        from: data.email,
+        to: process.env.emailAddress,
+        subject: `Message from ${data.name}`,
+        html: `
+          <h3>Contact Details</h3>
+          <ul>
+            <li>Name: ${data.name}</li>
+            <li>Email: ${data.email}</li>
+            <li>PhoneNo: ${data.number}</li>
+            <li>Message: ${data.message}</li>
+          </ul>
+        `
+    }
+
+    transporter.sendMail( mailOptions, (error, response) => {
+        if( error ) {
+            res.send("Your Message has been Successfully Sent")
+        } else {
+            res.send("Your Message has been sent Successfully")
+        }
+
+    }) 
+
+    transporter.close()
 
     
 })
